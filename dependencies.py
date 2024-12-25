2773199252f4d8b54a8e5f5b3c5e11378e4a122b
Michilis <120072772+Michilis@users.noreply.github.com>
2024-12-25 15:44:18 +0100

    Update dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from database import SessionLocal
 from models import Moderator
 import os
+import logging
 
 def get_db():
     db = SessionLocal()
@@ -13,6 +14,9 @@ def get_db():
 
 def get_api_key(x_api_key: str = Header(...), db: Session = Depends(get_db), admin_only: bool = False):
     admin_key = os.getenv("ADMIN_API_KEY")
+    logging.info(f"Received x-api-key: {x_api_key}")
+    logging.info(f"Expected ADMIN_API_KEY: {admin_key}")
+
     if x_api_key == admin_key:
         return True
 
