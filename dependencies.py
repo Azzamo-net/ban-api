f8fcd1eb17cc338ab4674912df5f6cbe7777d88f
Michilis <120072772+Michilis@users.noreply.github.com>
2024-12-30 00:56:00 +0100

    Update dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from database import SessionLocal
 from models import Moderator
 import os
+import logging
 
 def get_db():
     db = SessionLocal()
@@ -11,10 +12,16 @@ def get_db():
     finally:
         db.close()
 
-def get_api_key(x_api_key: str = Header(...), db: Session = Depends(get_db)):
+def get_api_key(x_api_key: str = Header(...), admin_only: bool = False, db: Session = Depends(get_db)):
     admin_key = os.getenv("ADMIN_API_KEY")
-    if x_api_key == admin_key:
-        return True
+    logging.info(f"Received x-api-key: {x_api_key}")
+    logging.info(f"Expected ADMIN_API_KEY: {admin_key}")
+
+    if admin_only:
+        if x_api_key == admin_key:
+            return True
+        else:
+            raise HTTPException(status_code=403, detail="Invalid API key for admin access")
 
     # Check if the key is a valid moderator key
     moderator = db.query(Moderator).filter(Moderator.private_key == x_api_key).first()
