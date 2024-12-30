99bdb42f9187cc2f2877521179a467b949aa7a4d
Michilis <120072772+Michilis@users.noreply.github.com>
2024-12-30 01:01:27 +0100

    Update dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -17,11 +17,12 @@ def get_api_key(x_api_key: str = Header(...), admin_only: bool = False, db: Sess
     logging.info(f"Received x-api-key: {x_api_key}")
     logging.info(f"Expected ADMIN_API_KEY: {admin_key}")
 
+    # Allow admin key for all endpoints
+    if x_api_key == admin_key:
+        return True
+
     if admin_only:
-        if x_api_key == admin_key:
-            return True
-        else:
-            raise HTTPException(status_code=403, detail="Invalid API key for admin access")
+        raise HTTPException(status_code=403, detail="Invalid API key for admin access")
 
     # Check if the key is a valid moderator key
     moderator = db.query(Moderator).filter(Moderator.private_key == x_api_key).first()
